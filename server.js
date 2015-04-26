ce42ef5e04addde0ee22dce7f41824c02150332b
Ben Stone <benjamin.v.stone@gmail.com>
2015-04-25 21:10:26 -0700

    Add basic server route and initialize angular

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 var express = require('express');
-var path    = require('path');
-
 var app = express();
 
-// routes 
-app.get('/', function(req, res) {
-  res.send('Hello World!');
-});
+var port  = process.env.PORT || 3000;
 
-var server = app.listen(3000, function() {
-  var port = server.address().port;
-  
-  console.log('Server listening on port: ', port);
-});
+app.use(express.static(__dirname + '/client'));
+
+// import routes 
+require('./server/routes')(app);
+
+app.listen(port);
+console.log('Server listening on port:', port);
+
+exports = module.exports = app;
