afa13e2ec7655cb4545633a183f666b6b9b2e390
Ben Stone <benjamin.v.stone@gmail.com>
2015-04-25 21:47:44 -0700

    Add support for HTML5 mode

--- client/app.js
+++ client/app.js
@@ -1,3 +1,21 @@
 angular.module('myApp', [
-  'ui-router'  
-]);
+  'ui.router'  
+])
+
+.config(function($stateProvider, $locationProvider) {
+  $locationProvider.html5Mode({
+    enabled: true,
+    requireBase: false
+  });
+    
+  $stateProvider
+    .state('login', {
+      url: '/login',
+      template: '<h2>Provided by router</h2>' // templateUrl: ./views/login.html
+    });
+    
+    
+})
+
+;
+
